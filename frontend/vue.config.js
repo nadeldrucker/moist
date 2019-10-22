06bdaa1cc0b25090cdde15f87c8607ba5f1141e9
Paul Orlob <orlobpaul@gmail.com>
2019-10-22 16:55:47 +0200

    Restructured project

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': __dirname + '/src/main'
-            }
-        },
-        entry: {
-            app: './src/main/main.js'
-        }
-    },
     css: {
         loaderOptions: {
             sass: {
